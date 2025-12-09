fff105d1f289fb89173a2c8a2ac38e84e2afde2d
Davi Marques <davidjdbm17@gmail.com>
2025-12-09 00:09:08 -0300

    Model feito

--- src/main/java/model/ModelTarefas.java
+++ src/main/java/model/ModelTarefas.java
@@ -9,5 +9,35 @@ package model;
  * @author Thiag
  */
 public class ModelTarefas {
+    // Atributos 
+    private int id;
+    private String titulo;
+    private String descricao;
     
+    //Construtor
+    public ModelTarefas (int id, String titulo, String descricao) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descricao = descricao;
+    }
+    // Getter
+    public int getid() {
+        return id;
+    }
+    public String gettitulo () {
+        return titulo;
+    }
+    public String getdescricao () {
+        return descricao;
+    }
+    // Setters
+    public void setid (int id) {
+        this.id = id;
+    }
+    public void settitulo (String titulo) {
+        this.titulo = titulo;
+    }
+    public void setdescricao (String descricao) {
+        this.descricao = descricao;
+    }
 }
